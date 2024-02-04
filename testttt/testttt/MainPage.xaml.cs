d70ae4cedd688e324bffa4188c19fa5fcbc12add
juicebyjustin <2144123+juicebyjustin@users.noreply.github.com>
2024-02-04 07:20:30 -0600

    initial commit. Label ActionTagTapped event broken on iOS

---
+++ testttt/testttt/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace testttt
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            PhoneLabel.ActionTagTapped += ActionTagTapped;
+            EmailLabel.ActionTagTapped += ActionTagTapped;
+        }
+
+        private void ActionTagTapped(object sender, Forms9Patch.ActionTagEventArgs e)
+        {
+            Forms9Patch.Toast.Create("Link Activated", "The link (id: " + e.Id + ", href:" + e.Href + ") was activated.");
+        }
+    }
+}
+
